b363417a6fc445dfe429f882820e50a23f6778cf
Amrhub <amr_ahmed65@hotmail.com>
2021-11-16 19:14:08 +0200

    add footer

--- src/index.js
+++ src/index.js
@@ -70,3 +70,13 @@ for (let x = 0; x < arrayOfDishes.length; x += 1) {
         </div>
     </div>`;
 }
+const renderFooter = () => {
+  const footer = document.createElement('footer');
+  const footerContent = 'Created by Microverse under CC license';
+
+  footer.classList.add('footer');
+  footer.innerHTML = `<p class="footer__content">${footerContent}</p>`;
+  document.body.appendChild(footer);
+};
+
+renderFooter();

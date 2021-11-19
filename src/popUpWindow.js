ec3c12d15800e89202fa722937187169660d76eb
Rafael Echart <rafaelechartsaltos@gmail.com>
2021-11-19 16:09:14 -0500

    CR align suggestions

--- src/popUpWindow.js
+++ src/popUpWindow.js
@@ -133,11 +133,14 @@ const displayPopUpCommentWindow = async (dishIdMeal) => {
   
   
 </div>
-  <section class="meals-comments dflex-column-alignCenter-justifyCenter"> 
-    <h3 class="comments-header"></h3>
+  <section class="meals-comments"> 
+    <h3 class="comments-header text-align-h3"></h3>
+    <div class="newAlign">
     <ul class="comments-list dflex-column-alignCenter-justifyFlexStart" id="commentBox"></ul>
+    </div>
   </section>
   <h3>Add a comment</h3>
+  <div class="newWidth">
   <form class="comments-form dflex-column-alignCenter-justifyCenter">
   <label for="comment-name" hidden>Name</label>
   <input type="text" maxlength="250" id="comment-name" placeholder="Your name" required>
@@ -145,7 +148,8 @@ const displayPopUpCommentWindow = async (dishIdMeal) => {
   <textarea id="comment-text" placeholder="Your comment" required></textarea>
   <button type="submit" id="newcomment-button" class="comments-form__submit SeeCommentsButton"><div id="loading-newcomment" class="loading-newcomment">
   <div class="loader-comment">Loading...</div>
-</div><span id="new-comment-text">Comment</span></button>
+  </div><span id="new-comment-text">Comment</span></button>
+  </div>
   `;
 
   innerMenu();
